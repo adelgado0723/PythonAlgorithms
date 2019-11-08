012e2231a7ad46dcd92b7984840526c220d7fbd5
Andres Delgado <adelgado0723@gmail.com>
2019-11-08 00:14:56 -0500

    Added find and find_parent functions. Working on remove

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,7 +4,7 @@ import BinaryNode
 class BinaryTree:
 
     def __init__(self):
-            self.root = None
+        self.root = None
 
     def add(self, value):
         if(self.root is None):
@@ -27,7 +27,10 @@ class BinaryTree:
             node.count += 1
 
     def print_value(self, node):
-        print(f"Value: {node.value} - Count: {node.count}")
+        if(node is not None and node.value is not None):
+            print(f"Value: {node.value} - Count: {node.count}")
+        else:
+            print("Either the node or its value is None")
 
     def print_in_order(self):
         self._traverse_in_order(self.root, self.print_value)
@@ -39,7 +42,7 @@ class BinaryTree:
             self._traverse_in_order(node.right, fn)
 
     def print_pre_order(self):
-          self._traverse_pre_order(self.root, self.print_value)
+        self._traverse_pre_order(self.root, self.print_value)
 
     def _traverse_pre_order(self, node, fn):
         if(node is not None):
@@ -48,7 +51,7 @@ class BinaryTree:
             self._traverse_pre_order(node.right, fn)
 
     def print_post_order(self):
-          self._traverse_post_order(self.root, self.print_value)
+        self._traverse_post_order(self.root, self.print_value)
 
     def _traverse_post_order(self, node, fn):
         if(node is not None):
@@ -56,47 +59,101 @@ class BinaryTree:
             self._traverse_post_order(node.right, fn)
             fn(node)
 
-
     def find_max_node(self):
-      if(self.root is not None):
-        return self._find_max_node(self.root)
+        if(self.root is not None):
+            return self._find_max_node(self.root)
 
     def _find_max_node(self, node):
-      if(node.right is None):
-        return node
-      else:
-        return self._find_max_node(node.right)
+        if(node.right is None):
+            return node
+        else:
+            return self._find_max_node(node.right)
 
     def find_min_node(self):
-      if(self.root is not None):
-        return self._find_min_node(self.root)
+        if(self.root is not None):
+            return self._find_min_node(self.root)
 
     def _find_min_node(self, node):
-      if(node.left is None):
-        return node
-      else:
-        return self._find_min_node(node.left)
+        if(node.left is None):
+            return node
+        else:
+            return self._find_min_node(node.left)
+
+    def find_parent(self, value):
+        return self._find_parent(value, self.root)
+
+    def _find_parent(self, value, node):
+        if(node is not None):
+            if(node.left is not None and node.left.value == value):
+                return node
+            elif(node.right is not None and node.right.value == value):
+                return node
+
+            if (node.value > value):
+                return self._find_parent(value, node.left)
+
+            if (node.value < value):
+                return self._find_parent(value, node.right)
+
+    def find(self, value):
+        return self._find(value, self.root)
+
+    def _find(self, value, node):
+        if(node is not None):
+            if (node.value == value):
+                return node
+            if (node.value > value):
+                return self._find(value, node.left)
+
+            if (node.value < value):
+                return self._find(value, node.right)
 
     # Remove Node Given a Value
+    def remove(self, value):
+        if(self.root is not None):
+            return self.remove(value, self.root)
 
-    # Find if there is a node in the tree with that value
-      # If not, just return
+    def _remove(self, value, node):
 
-    # If the count is greater than 1
-      # Decrement the count
-    
-    #Else
-        # If it is a leaf: 
+        # Find if there is a node in the tree with that value
+        # If not, just return
+        to_be_removed = self._find(value, node)
+
+        if(to_be_removed is None):
+            return
+
+        # If the count is greater than 1, decrement the count
+        if(to_be_removed.count > 1):
+            to_be_removed.count -= 1
+            return
+
+        # Fetch the parent
+        parent = self._find_parent(value, node)
+        is_left_child = False
+        if(parent is None):
+          # to_be_removed is the root
+        else:
+          # Is to_be_removed the left or right child
+            if (parent.left.value == to_be_removed.value):
+                is_left_child = True
+
+        # If it is a leaf:
+        if (to_be_removed.left is None and to_be_removed.right is None):
             # set the parent's reference to None
-        # Else If it has a right child
+            if(is_left_child):
+                parent.left = None
+            else:
+                parent.right = None
+
+            # Else If it has a right child
             # Set the parent's reference to the right child sub-tree
-        # Else If it has a left child
+            # Else If it has a left child
             # Set the parent's reference to the left child sub-tree
-        # Else if it has two children
+            # Else if it has two children
             # Set the parent's reference to the left child sub-tree
-            # then, find the max node in that left sub-tree and 
+            # then, find the max node in that left sub-tree and
             # attach the right sub-tree to that node's right reference
-    
+
 
 def main():
     tree = BinaryTree()
@@ -115,17 +172,27 @@ def main():
     tree.add(46)
     tree.add(46)
 
-    print("Printing In Order:")
-    tree.print_in_order()
-    print("\nPrinting Pre Order:")
-    tree.print_pre_order()
-    print("\nPrinting Post Order:")
-    tree.print_post_order()
+    # print("Printing In Order:")
+    # tree.print_in_order()
+    # print("\nPrinting Pre Order:")
+    # tree.print_pre_order()
+    # print("\nPrinting Post Order:")
+    # tree.print_post_order()
 
-    print("\nHighest Value = ")
-    tree.print_value(tree.find_max_node());
+    # print("\nHighest Value = ")
+    # tree.print_value(tree.find_max_node())
 
-    print("\nLowest Value = ")
-    tree.print_value(tree.find_min_node());
+    # print("\nLowest Value = ")
+    # tree.print_value(tree.find_min_node())
+
+    tree.print_value(tree.find(46))
+    tree.print_value(tree.find(10))
+
+    empty_tree = BinaryTree()
+
+    empty_tree.print_value(empty_tree.find(10))
+    empty_tree.print_value(empty_tree.find_max_node())
+    empty_tree.print_value(empty_tree.find_min_node())
+
 
 main()
